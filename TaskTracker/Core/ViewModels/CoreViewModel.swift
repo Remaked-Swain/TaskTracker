9fcf4ed429d63222b30ff66de47a92b51ec0b7e2
YunSwain <destap@naver.com>
2023-07-19 23:42:44 +0900

    CoreViewModel logic updated

--- TaskTracker/Core/ViewModels/CoreViewModel.swift
+++ TaskTracker/Core/ViewModels/CoreViewModel.swift
@@ -31,8 +31,10 @@ class CoreViewModel: ObservableObject {
         // 기존 Tasks 중 같은 id 를 가진 할 일이 존재하면 덮어쓰기, 없으면 추가
         if let index = allTasks.firstIndex(where: {$0.id == task.id}) {
             allTasks[index] = task
+            taskDataService.updateTask(task)
         } else {
             allTasks.append(task)
+            taskDataService.createTask(task)
         }
         
         fetchTasks()
@@ -48,10 +50,12 @@ class CoreViewModel: ObservableObject {
         // 동일한 이름의 카테고리 무한생성 방지,
         guard allCategories.contains(category) == false else { return }
         allCategories.append(category)
+        categoryDataService.createCategory(category)
     }
     
     func deleteCategory(category: String) {
         guard let index = allCategories.firstIndex(of: category) else { return }
         allCategories.remove(at: index)
+        categoryDataService.deleteCategory(category)
     }
 }
